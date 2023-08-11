61e3721810e5557f55cffb60e63cb628a76930fa
Nandinim17 <139409440+Nandinim17@users.noreply.github.com>
2023-08-12 01:31:01 +1000

    change

---
+++ Glossary/Data.js
@@ -0,0 +1,123 @@
+const glossary = [{
+    id:'1',
+    term:'Devops',
+    definition:'Devops is the bunch of software and delivery packets which takes atmost care between softwareand IT actions. it is most useful to shorten the life cycle of softwae development.',
+    references:'https://www.dynatrace.com/monitoring/solutions/devops-wp/?utm_source=google&utm_medium=cpc&utm_term=what%20is%20devops&utm_campaign=anz--devops-devops&utm_content=none&utm_campaign_id=15353438761&gclsrc=aw.ds&gclid=Cj0KCQjwn_OlBhDhARIsAG2y6zMCrK7t0tkBz9Rkqj6bktZupmx_vTvErWKKTZeZcm8RKPNLmYIGkIYaAsYCEALw_wcB'
+},
+{
+    id:'2',
+    term:'Continuous testing and quality assurance',
+    definition:'Testing each and every step with quality in development life cycle is called testing and maintaing a prefered quality in the service of a product is known as quality assurance',
+    references:'https://www.globalapptesting.com/blog/what-is-continuous-testing#:~:text=Continuous%20testing%20in%20DevOps%20is,by%20testing%20early%20and%20often.'
+},
+{
+    id:'3',
+    term:'Automated delivery pipeline',
+    definition:'It is the bunch of tools that are used by development and operation section for compling, constructing, testing and deploying the code easire and faster.',
+    references:'https://www.simplilearn.com/what-is-pipeline-in-devops-tools-implementation-article#:~:text=A%20DevOps%20pipeline%20is%20the,more%20like%20an%20assembly%20line.'
+},
+{
+   id:'4',
+   term:'Configuration management',
+   definition:'Configuration management mentions the process by which the environment hosting software are made and maintained.',
+   references:'https://www.browserstack.com/guide/configuration-management-in-devops#:~:text=DevOps%20Configuration%20Management%20automates%20mundane,the%20organization%20as%20a%20whole.'
+},
+{
+    id:'5',
+    term:'Infrastructure of code',
+    definition:'It is storded inside the version control system to become the part of project.',
+    references:'https://spacelift.io/blog/infrastructure-as-code'
+ },
+ {
+        id:'6',
+        term:'Version control ',
+        definition:'Its another name is corce control, it does tracking and managing of the codes.',
+        references:'https://www.atlassian.com/git/tutorials/what-is-version-control'
+ },
+ {
+    id:'7',
+    term:'Microservices',
+    definition:'It is the designed process of making a seperate application to different services that performs specific function and talks to web.',
+    references:'https://learn.microsoft.com/en-us/devops/deliver/what-are-microservices'
+},
+{
+    id:'8',
+    term:'Kubernetes',
+    definition:'The trials that kubernetes provide is useful for creating, deploying and scaling enterprise grade of pipelines',
+    references:'https://intellipaat.com/blog/kubernetes-and-devops/?US#:~:text=What%20is%20the%20use%20of,that%20are%20involved%20in%20orchestration.'
+},
+{
+    id:'9',
+    term:'Infrastructure automation ',
+    definition:'It helps teams to manage workload more efficiently and quickly. It provides automation to all processes.',
+    references:'https://spacelift.io/blog/devops-automation-tools'
+},
+{
+    id:'10',
+    term:'Containers',
+    definition:'It is a form of operating system. A single container is enough to run microservers to large applications.',
+    references:'https://www.netapp.com/devops-solutions/what-are-containers/#:~:text=DevOps%20teams%20know%20applications%20in,of%20where%20they%20are%20deployed.&text=Containers%20allow%20applications%20to%20be,deployed%2C%20patched%2C%20or%20scaled.&text=Containers%20support%20agile%20and%20DevOps,%2C%20test%2C%20and%20production%20cycles.'
+},
+{
+    id:'11',
+    term:'Create with end in mind',
+    definition:'This focuses on the requirements of the customer and creatinf product or service that solves real world problems.',
+    references:'https://www.atlassian.com/devops/what-is-devops#:~:text=Customer%2Dcentric%20action,services%20centered%20around%20user%20needs.'
+},
+{
+    id:'12',
+    term:'DevOps principles',
+    definition:'This principles helps to decrease the complexity, instant problem solution and continuous delivery. It deploys higher quality code as compared to older teams.',
+    references:'https://www.atlassian.com/devops/what-is-devops#:~:text=Benefits%20of%20DevOps,-There%20are%20three&text=The%20technical%20benefits%20include%20reduced,efficient%20teams%2C%20and%20happier%20customers.'
+},
+{
+    id:'13',
+    term:'DevOps cycle',
+    definition:'It is a continuous lifecycle development that employs practice to build, plan, monitor, operate and constant feedbackthrough software lifecycle.',
+    references:"https://www.browserstack.com/guide/devops-lifecycle#:~:text=The%20DevOps%20lifecycle%20is%20a,feedback%20throughout%20the%20software's%20lifecycle."
+},
+{
+    id:'14',
+    term:'DevOps culture',
+    definition:'It is all about divided understanding between operations, developers and responsibilities for the software they build.',
+    references:'https://www.atlassian.com/team-playbook/examples/devops-culture#:~:text=DevOps%20culture%20is%20all%20about,%2C%20and%20%22the%20business%22.'
+},
+{
+    id:'15',
+    term:'Continuous integration',
+    definition:'It is a development process where team use version control system and integrate their work in the same location as a main branch.',
+    references:'https://docs.aws.amazon.com/codepipeline/latest/userguide/concepts-continuous-delivery-integration.html'
+},
+{
+    id:'16',
+    term:'Continuous delivery',
+    definition:'It is a development methodology where the release process is automatic built, tested and deployed.',
+    references:'https://docs.aws.amazon.com/codepipeline/latest/userguide/concepts-continuous-delivery-integration.html'
+},
+{
+    id:'17',
+    term:'Continuous monitoring',
+    definition:'It is also known as continuous control monitoring is automatic process in which DevOps can observe and detect complicated issues and security threats in each phase.',
+    references:'https://www.browserstack.com/guide/continuous-monitoring-in-devops#:~:text=Fundamentally%2C%20Continuous%20Monitoring%20(CM),phase%20of%20the%20DevOps%20pipeline.'
+},
+{
+    id:'18',
+    term:'Collaboration',
+    definition:'It is data of development and operation which are working combinedly to solve problems as early as possible keeping customers in mind.',
+    references:'https://www.softwaretestinghelp.com/collaboration-in-devops/#:~:text=So%2C%20collaboration%20in%20the%20DevOps,the%20customer%20focus%20in%20mind.'
+},
+{
+    id:'19',
+    term:'Cross functional autonomous teams',
+    definition:'It is a group of people with individual skills, which can be from HR, IT, Marketing domain or finance.',
+    references:'https://www.plutora.com/blog/cross-functional-teams#:~:text=Bridge%20the%20gap%20between%20DevOps%20and%20Agile%20with%20Plutora&text=A%20cross%2Dfunctional%20team%20consists,human%20resources%2C%20or%20marketing%20domains.'
+},
+{
+    id:'20',
+    term:'Customer centric action',
+    definition:'this team provide small description of the conversation to the customer and end user to develop service and product.',
+    references:'https://www.atlassian.com/devops/what-is-devops#:~:text=Customer%2Dcentric%20action,services%20centered%20around%20user%20needs.'
+}
+]
+
+module.exports = glossary
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var listofass = require('../Glossary/Data') 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SIT722 DEVOPS GLOSSARY', list: listofass });
 });
 
 module.exports = router;
